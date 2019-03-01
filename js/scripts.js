0d85dac9b3889de39df9a9b77a64a3afe06bd587
yontey <muasajohn01@gmail.com>
2019-03-01 14:51:34 +0300

    added javascript functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-var dice = Math.floor(Math.random()*6);
-function roll(){
-
-   document.getElementById("roll1").innerHTML=dice;
-   document.getElementById("roll2").innerHTML=dice;
-   document.getElementById("roll3").innerHTML=dice;
-
+var dice = Math.floor(Math.random()*6)+1;
+function roll1(){
+  document.getElementById("roll1").innerHTML=dice;
+}
+var dice1 = Math.floor(Math.random()*6)+1;
+function roll2() {
+    document.getElementById("roll2").innerHTML=dice1;
+}
+var dice2 = Math.floor(Math.random()*6)+1;
+function roll3() {
+    document.getElementById("roll3").innerHTML=dice2;
+}
+var dice3 = Math.floor(Math.random()*6)+1;
+function roll4() {
+    document.getElementById("roll4").innerHTML=dice3;
+}
+var dice4 = Math.floor(Math.random()*6)+1;
+function roll5() {
+    document.getElementById("roll5").innerHTML=dice4;
+}
+var dice5 = Math.floor(Math.random()*6)+1;
+function roll6() {
+    document.getElementById("roll6").innerHTML=dice5;
 }
