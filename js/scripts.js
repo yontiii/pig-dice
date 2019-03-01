d405988d40aa8c414945cce7faa4dabab5dc0f27
yontey <muasajohn01@gmail.com>
2019-03-01 13:59:21 +0300

    added initial page structure

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var dice = Math.floor(Math.random()*6);
+function roll(){
+
+   document.getElementById("roll1").innerHTML=dice;
+   document.getElementById("roll2").innerHTML=dice;
+   document.getElementById("roll3").innerHTML=dice;
+
+}
