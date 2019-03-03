2e015fba6d8e1ce3b95f7b3022e7f1d86c778c85
yontey <muasajohn01@gmail.com>
2019-03-03 21:33:10 +0300

    added the staart and instructions link

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
+
 var scores = [];
 var play = 0;
 
 function playerOne() {
- var run = Math.floor(Math.random() * 6) + 1;
+  var run = Math.floor(Math.random() * 6) + 1;
   document.getElementById("roll1").innerHTML = run;
   scores.push(run);
-  for (i = 0; i < scores.length; i++) {
-    play += scores[i];
+  if (scores == 1) {
+    alert("stop")
   }
-  document.getElementById("woke").innerHTML=play;
+  else {
+    for (i = 0; i < scores.length; i++) {
+      play += scores[i];
+    }
+    document.getElementById("woke").innerHTML = play;
+  }
+
 }
